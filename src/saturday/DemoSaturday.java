4d01fd51b4a6e0f359cade6ee351a5a6dfbd287f
hirah <hirah@DESKTOP-M75OOQ6.myfiosgateway.com>
2021-10-02 10:36:39 -0400

    added xyz code

--- src/saturday/DemoSaturday.java
+++ src/saturday/DemoSaturday.java
@@ -1,5 +1,9 @@
 package saturday;
 
 public class DemoSaturday {
+	public static void main(String[] args) {
+		
+		System.out.println("Saurday Demo....");
+	}
 
 }
--- src/saturday/DemoSunday.java
+++ src/saturday/DemoSunday.java
@@ -1,5 +1,11 @@
 package saturday;
 
 public class DemoSunday {
+	public static void main(String[] args) {
+		
+		System.out.println("Sunday Demo....");
+		
+		System.out.println("xyz..... code");
+	}
 
 }
